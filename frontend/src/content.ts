44674444b676ace35bc37710924a4105db44f87d
Cookie Biscuit <oo222515@gmail.com>
2026-02-13 10:05:01 -0600

    checking fix for manifest for firefox support and added content.js for job post scraping

---
+++ frontend/src/content.ts
@@ -0,0 +1,21 @@
+const url = new URL(window.location.href);
+
+if (url.href === "https://www.linkedin.com/") {
+    function getJobId () {
+        return new URLSearchParams(window.location.search).get("currentJobId");
+    }
+
+    function getJobData() {
+        const jobId = getJobId();
+        if (!jobId) return null;
+
+        const url = new URL(`https://www.linkedin.com/jobs/view/${jobId}`);
+
+        return {
+            jobId,
+            url,
+            title: url.searchParams.get("title"),
+            company: url.searchParams.get("company"),
+        };
+    }
+}
